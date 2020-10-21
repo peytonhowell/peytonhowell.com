51239afae6dfdfdf7e9f3cf58891c88f4c1890fb
peytonhowell <peytonhowelle@gmail.com>
2020-10-20 22:43:46 -0400

    teest

---
+++ personalwebsite/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { mdiLinkedin } from '@mdi/js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'personalwebsite';
+}
